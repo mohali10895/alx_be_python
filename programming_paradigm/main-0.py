292d04969f630f8e4f6d63fde409faecd868bd42
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-10-05 20:31:04 +0300

    Update main-0.py

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -18,7 +18,7 @@ def main():
         account.deposit(amount)
     elif command == "withdraw" and amount is not None:
         if account.withdraw(amount):
-            print(f"Withdrew: ${amount:.1f}")  # Format output to one decimal place
+            print(f"Withdrew: ${amount}")  # Format output to one decimal place
         else:
             print("Insufficient funds.")
     elif command == "display":
