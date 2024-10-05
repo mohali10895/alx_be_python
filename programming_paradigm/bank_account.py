da1e749ed80182632220a698975f3389b82a0d82
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-10-05 20:30:24 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,7 +9,7 @@ class BankAccount:
         """Add the specified amount to the account balance."""
         if amount > 0:
             self.__account_balance += amount
-            print(f"Deposited: ${amount:.1f}")  # Format output to one decimal place
+            print(f"Deposited: ${amount}")  # Format output to one decimal place
         else:
             print("Deposit amount must be positive.")
 
